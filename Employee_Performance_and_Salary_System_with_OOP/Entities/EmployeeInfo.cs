22c8158618e24456c153e5cba2fd5ec8fdbb4c77
nahin <nurealamnahin16@gmail.com>
2026-04-27 20:16:14 +0600

    entities section complete

---
+++ Employee_Performance_and_Salary_System_with_OOP/Entities/EmployeeInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Performance_and_Salary_System_with_OOP.Entities
+{
+    class EmployeeInfo
+    {
+        private int _id;
+        private string _name;
+        private double _basicSalary;
+
+
+        private EmployeeType _EmployeeType;
+
+        public EmployeeInfo(int id, string name, double basicSalary, EmployeeType EmployeeType)
+        {
+            _id = id;
+            _name = name;
+            _basicSalary = basicSalary;
+            _EmployeeType = EmployeeType;
+
+        }
+
+
+        public int getId()
+        {
+            return _id;
+        }
+
+
+        public string getName()
+        {
+            return _name;
+        }
+
+
+        public double getSalary()
+        {
+            return _basicSalary;
+        }
+
+
+        public EmployeeType getType()
+        {
+            return _EmployeeType;
+        }
+
+    }
+}
---
+++ Employee_Performance_and_Salary_System_with_OOP/Entities/EmployeeType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Performance_and_Salary_System_with_OOP.Entities
+{
+    class EmployeeType
+    {
+        private string _employeeType;
+
+
+        public EmployeeType(string empType)
+        {
+            this._employeeType = empType;
+        }
+
+
+        public string getEmployeeType()
+        {
+            return _employeeType;
+        }
+
+    }
+}
